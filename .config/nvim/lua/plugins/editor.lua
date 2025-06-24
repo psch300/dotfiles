2daf827e56b3c36efe6bb240fb94dee6bd9d8eed
Sungchang Park <psch300@gmail.com>
2025-06-24 12:05:56 +0900

    initialize

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,118 @@
+return {
+  -- Hihglight colors
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = {},
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+    keys = {
+      {
+        ";f",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.find_files({
+            no_ignore = false,
+            hidden = true,
+          })
+        end,
+        desc = "Lists files in your current working directory, respects .gitignore",
+      },
+      {
+        ";r",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.live_grep()
+        end,
+        desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
+      },
+      {
+        "\\\\",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.buffers()
+        end,
+        desc = "Lists open buffers",
+      },
+      {
+        ";;",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.resume()
+        end,
+        desc = "Resume the previous telescope picker",
+      },
+      {
+        ";e",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.diagnostics()
+        end,
+        desc = "Lists Diagnostics for all open buffers or a specific buffer",
+      },
+      {
+        ";s",
+        function()
+          local builtin = require("telescope.builtin")
+          builtin.treesitter()
+        end,
+        desc = "Lists Function names, variables, from Treesitter",
+      },
+      {
+        "sf",
+        function()
+          local telescope = require("telescope")
+
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# dotfiles
+
+This is dotfiles for my work. I will keep updating this repository.
+
+## Contents
+
+- Neovim config
+
+## Requirements
+
+- Warp
+- Nerd Font
+- Neovim
+- ripgrep
+- LazyGit
